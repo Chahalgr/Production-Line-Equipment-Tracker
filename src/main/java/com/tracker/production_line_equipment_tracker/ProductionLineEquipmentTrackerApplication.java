f2efc26d4b10b3e357087e2fcbce248fab3ad351
Chahalgr <gurleensingh94647@gmail.com>
2026-03-01 10:54:21 -0600

    Initial commit

---
+++ src/main/java/com/tracker/production_line_equipment_tracker/ProductionLineEquipmentTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.production_line_equipment_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionLineEquipmentTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductionLineEquipmentTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tracker/production_line_equipment_tracker/ProductionLineEquipmentTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.production_line_equipment_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionLineEquipmentTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
